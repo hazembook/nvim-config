cc8dadc5fbb235bb419101cea3b4f8fdd23a404f
Hazem Shaban <hazem.shaban.std@gmail.com>
2023-11-13 02:07:09 +0200

    Update mappings

--- lua/hazembook/remap.lua
+++ lua/hazembook/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>xe", vim.cmd.bdelete)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>wq", vim.cmd.wq)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
