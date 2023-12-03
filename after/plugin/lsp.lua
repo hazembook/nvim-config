3eedaf6e70d821b3b902bf940e888f363d1ee4e9
Hazem Shaban <hazem.shaban.std@gmail.com>
2023-12-03 20:04:42 +0200

    Use sign icons for lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,12 @@
 local lsp = require('lsp-zero').preset({})
 
+lsp.set_sign_icons({
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
+})
+
 lsp.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
