e109c6e57083e630c0d73c496605aa788e58b523
Hazem Shaban <hazem.shaban.std@gmail.com>
2024-01-13 18:41:24 +0200

    Integrate an AI code assistant

--- lua/hazembook/lazy.lua
+++ lua/hazembook/lazy.lua
@@ -121,5 +121,19 @@ require('lazy').setup({
         opts = {
             -- options
         },
+    },
+
+    {
+        'Exafunction/codeium.vim',
+        event = 'BufEnter',
+        config = function()
+            -- Change '<C-g>' here to any keycode you like.
+            vim.keymap.set('i', '<C-y>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+            vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+                { expr = true, silent = true })
+            vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        end
     }
 })
