dca85eb94b9bc51555e704b30ad406d6999d8b33
Hazem Shaban <hazem.shaban.std@gmail.com>
2024-07-20 17:10:24 +0200

    Update telescope tag

--- lua/hazembook/lazy.lua
+++ lua/hazembook/lazy.lua
@@ -19,7 +19,7 @@ require('lazy').setup({
 
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
+        tag = '0.1.8',
         -- or                            , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -140,5 +140,14 @@ require('lazy').setup({
             'stevearc/dressing.nvim', -- optional for vim.ui.select
         },
         config = true,
+    },
+    {
+        "lervag/vimtex",
+        lazy = false, -- we don't want to lazy load VimTeX
+        -- tag = "v2.15", -- uncomment to pin to a specific release
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end
     },
 })
