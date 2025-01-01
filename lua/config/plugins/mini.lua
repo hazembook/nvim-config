fc09361fa0443d3035eae5f3faba11b16cb60b2a
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-01-01 22:48:55 +0200

    Enable Autopairs

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -4,7 +4,9 @@ return {
     enabled = true,
     config = function()
       local statusline = require('mini.statusline')
+      local pairs = require("mini.pairs")
       statusline.setup { use_icons = true }
+      pairs.setup {}
     end
   },
 }
