9538d8963e4384b8cbf0ab0d1e59a4e20b14ca3e
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-01-03 21:10:38 +0200

    Easier blink keymaps for Termux

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -6,14 +6,25 @@ return {
     version = '*',
 
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+
+        ['<Tab>'] = { 'accept', 'fallback' },
+      },
 
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
 
-      signature = { enabled = true }
+      signature = { enabled = true },
+
+      completion = {
+        documentation = { auto_show = true }
+      },
     },
   },
 }
