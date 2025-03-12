f9dead461a62a15393a3c854cc7001993e3d05d7
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-03-12 06:36:36 +0200

    Uncomment keybindings

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -49,9 +49,9 @@ return {
           vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
           vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
           vim.keymap.set("n", "<leader>vf", function() vim.diagnostic.open_float() end, opts)
-          -- vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-          -- vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-          -- vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+          vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+          vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+          vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
           vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end,
       })
