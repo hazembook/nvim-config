46ca8f63a86d797434b38391c66428a11c029f70
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-06-24 15:36:08 +0200

    testing a debuger

---
+++ lua/config/plugins/debuger.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+
+      require("dap-go").setup()
+      require("dapui").setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<leader>dc", dap.continue, {})
+      vim.keymap.set("n", "<leader>dr", function() require('dapui').open({ reset = true }) end, {})
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python3"
+      require("dap-python").setup(path)
+    end,
+  },
+}
