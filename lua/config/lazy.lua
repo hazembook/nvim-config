4fd440bfda1f579cc6d36fc4eb849194356edcd2
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-09-25 17:07:59 +0200

    stop lazy notifications when changes are detected

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,4 +32,6 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  -- detect changes without notifying
+  change_detection = { enabled = true, notify = false },
 })
