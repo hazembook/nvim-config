3cdcc6bcaf6a57fb8b6db69711f1ac64ac044c2b
Hazem Shaban <hazem.shaban.std@gmail.com>
2025-11-01 02:08:38 +0200

    pleasant autopairs after adding code block insertion keymap

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,12 +4,12 @@ return {
     enabled = true,
     config = function()
       local statusline = require('mini.statusline')
-      -- local pairs = require("mini.pairs")
+      local pairs = require("mini.pairs")
       local surround = require('mini.surround')
       local align = require('mini.align')
 
       statusline.setup { use_icons = true }
-      -- pairs.setup {}
+      pairs.setup {}
       surround.setup {}
       align.setup {}
     end
