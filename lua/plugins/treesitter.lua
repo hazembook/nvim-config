b10b6af1f5a1d5dde8c5c61d3b761052e8b1db2a
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-03 13:53:45 +0200

    fix: pin nvim-treesitter to main branch for 0.12 compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
+    branch = "main",
     build = ":TSUpdate",
     -- In Neovim 0.12, we no longer need the massive setup block.
     -- This plugin is now just here to provide the :TSInstall commands.
