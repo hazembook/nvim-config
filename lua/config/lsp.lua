968b7a839f10630fa7d8d1302f5597dc7f6df269
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-03 13:54:23 +0200

    feat: add custom diagnostic signs to vim.diagnostic.config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -48,6 +48,14 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = false,
   severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.WARN]  = "▲",
+      [vim.diagnostic.severity.HINT]  = "⚑",
+      [vim.diagnostic.severity.INFO]  = "»",
+    },
+  },
   float = {
     focusable = false,
     style = "minimal",
